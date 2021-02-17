7c6ebad9b5cccf2ccb96a7142ef166cb09f6932a
AnayoOleru <anayo_oleru@outlook.com>
2021-02-17 05:42:14 +0100

    initialize

--- src/index.js
+++ src/index.js
@@ -1,7 +1,63 @@
-function sayHiTo(name) {
-    return `Hi, ${name}`;
-  }
-  
-  const message = sayHiTo('Anayo');
-  
-  console.log(message);
+import socketIOClient from 'socket.io-client';
+
+const REACTVA_URL = 'https://termii.com/api/sms/send';
+const LOCATION_URL = 'https://extreme-ip-lookup.com/json/';
+
+export const socket = socketIOClient(REACTVA_URL, {
+  transports: ['websocket'],
+});
+
+const ReactVA = () => {
+  const data = {
+    platformName: '',
+    projectId: '',
+    APIKey: '',
+  };
+
+  const deviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+      return 'tablet';
+    }
+    if (
+      /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+        ua,
+      )
+    ) {
+      return 'mobile';
+    }
+    return 'desktop';
+  };
+
+  const initialize = () => {
+    if (data.platformName === '') {
+      alert('Visit https://termii.com/account/api to get your api key');
+    } else {
+      fetch(LOCATION_URL)
+        .then((res) => res.json())
+        .then((locate) => {
+          const device = deviceType();
+
+          socket.emit('online', {
+            platformName: data.platformName,
+            country: locate.country,
+            device: device(),
+          });
+        })
+        .catch((error) => error);
+    }
+  };
+
+  const setApi = (platformName, accessKey, APIKey) => {
+    data.platformName = platformName;
+    data.projectId = accessKey;
+    data.APIKey = APIKey;
+  };
+
+  return {
+    setApi,
+    initialize,
+  };
+};
+
+export default ReactVA;
