9eac75530702bc1a3161ec4599ba14c14bd375ff
AnayoOleru <anayo_oleru@outlook.com>
2021-02-17 06:34:19 +0100

    done

--- test/index.js
+++ test/index.js
@@ -1,8 +1,41 @@
 // Require the dev-dependencies
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import server from '../server';
+import nock from 'nock';
 
-const should = chai.should();
+import ReactVA from '../src';
+
+const { Should, expect } = chai;
 
 chai.use(chaiHttp);
+
+describe('First test', () => {
+  it('Should assert true to be true', () => {
+    expect(true).to.be.true;
+  });
+});
+
+// const expect = require('chai').expect;
+// const nock = require('nock');
+
+// const getUser = require('../index').getUser;
+// const response = require('./response');
+
+// describe('Get User tests', () => {
+//   beforeEach(() => {
+//     nock('https://api.github.com')
+//       .get('/users/octocat')
+//       .reply(200, response);
+//   });
+
+//   it('Get a user by username', () => getUser('octocat')
+//     .then((response) => {
+//       // expect an object back
+//       expect(typeof response).to.equal('object');
+
+//       // Test result of name, company and location for the response
+//       expect(response.name).to.equal('The Octocat');
+//       expect(response.company).to.equal('GitHub');
+//       expect(response.location).to.equal('San Francisco');
+//     }));
+// });
